2850028aeaa18e06f885d0767610b53dba66d6d7
David <dglover2007@gmail.com>
2018-08-02 05:48:24 +0700

    added payload code

--- steps/step5.js
+++ steps/step5.js
@@ -2,41 +2,52 @@
 // run in bash
 // run curl
 // + get headers
-// + 
+// + parsing payload
 //dependencies
 var http = require('http');
 var url = require('url');
-var stringDecoder = require('string_decoder').StringDecoder;
+var StringDecoder = require('string_decoder').StringDecoder;
 
 //************************************ //
 //1 create server with 2 deps which get sent down after every req
  var server = http.createServer(function(req,res){
-     
-// 2 create var to get  req url and parse it - inc querystring data 
+
+// 2 create var to get  req url and parse it - inc querystring data
  var parsedUrl = url.parse(req.url, true);
 // 2 var get untrimmed url path
  var path = parsedUrl.pathname;
 
-// 2 var to trim path with a regex to give clean url with no slashes etc  
+// 2 var to trim path with a regex to give clean url with no slashes etc
     var trimmedUrl = path.replace(/^\+|\/+$/g,'');
-    
+
 // 4 get query string as an object result added to output below
     var queryStringObject = parsedUrl.query;
-    
+
 // 3 get http method POST GET etc convert to upper case
     var method = req.method.toUpperCase();
-    
+
 // 5 get headers as an object  ********************************  5
     var headers = req.headers;
-    
-// 1 send response
-  res.end('Hello World!!!\n');
-    
-// 2 & 3 & 4 logout requested path
-// note ',' and not '+' at start of query string ** replaced below line in step 5
-//console.log('Request being served for path '+trimmedUrl+' using the method '+method+' with the query sting params', queryStringObject);
-// 5 new logout uses postman for testing ********************** 5
-console.log('request received with these headers ', headers);
+
+// 5 get any payload with new string StringDecoder to collect stream
+// uses node built in feature - streams to know when stream starts and ends
+// switch postman to post for this test
+    var decoder = new StringDecoder('utf-8');
+    // var to hold stream
+        var buffer = '';
+        // function to append buffer when data begins streaming in
+        req.on('data',function(data){
+            buffer += decoder.write(data)
+        });
+        // function for when data string ends
+        req.on('end',function(){
+            buffer += decoder.end();
+        // move responses into end function anc change logout to log payload
+        // 1 - 5 send response
+          res.end('Hello World!!!\n');
+          // 5 new logout uses postman for testing payload (buffer)= comma separated********************** 5
+          console.log('request received with these headers',buffer)
+      });
  });//end create server function
 //***************************************//
 // 1 start server - listen on port ????- give cb function to say listening
@@ -44,7 +55,7 @@ console.log('request received with these headers ', headers);
   var port = 8000;
   server.listen(port, function() {
    console.log('Server listening on port '+port);
-  }); 
+  });
  // end server listen function
 
 
